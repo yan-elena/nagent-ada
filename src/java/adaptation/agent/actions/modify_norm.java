47f56952f59e26eec4fd7483ce1c17242465dd04
Elena Yan <elena.yan@studio.unibo.it>
2025-04-08 17:21:57 +0200

    feat: adapt actions

---
+++ src/java/adaptation/agent/actions/modify_norm.java
@@ -0,0 +1,26 @@
+package adaptation.agent.actions;
+
+import adaptation.agent.ANormativeAg;
+import jason.asSemantics.DefaultInternalAction;
+import jason.asSemantics.TransitionSystem;
+import jason.asSemantics.Unifier;
+import jason.asSyntax.Literal;
+import jason.asSyntax.LogicalFormula;
+import jason.asSyntax.StringTerm;
+import jason.asSyntax.Term;
+
+/**
+ * An internal action for modify an existing norm in the normative engine of the agent.
+ */
+public class modify_norm extends DefaultInternalAction {
+    @Override
+    public Object execute(TransitionSystem ts, Unifier un, Term[] args) throws Exception {
+        var ag = (ANormativeAg) ts.getAg();
+        StringTerm id = (StringTerm) args[0];
+        Literal consequence = (Literal) args[1];
+        LogicalFormula activation = (LogicalFormula) args[2];
+        ag.getLogger().info("[Action] Modify norm - id: " + id + " with activation: " + activation + " consequence: " + consequence);
+        ag.getNPLAInterpreter().modifyNorm(id.getString(), consequence, activation);
+        return true;
+    }
+}
